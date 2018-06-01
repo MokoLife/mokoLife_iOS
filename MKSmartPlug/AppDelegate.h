e9e5bf73792c179ab9d7ec48da79b06500ddbcf7
Chengang <chengang@mokotechnology.com>
2018-06-01 14:44:11 +0800

    Initial Commit

---
+++ MKSmartPlug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKSmartPlug
+//
+//  Created by aa on 2018/6/1.
+//  Copyright © 2018年 MK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
